a6d42bb503ea63a44b7f647daf9c049c60b02e7e
Qasim <qr202@cantab.ac.uk>
2021-07-26 20:52:28 +0100

    remove AR screen navigation header + App.js code cleanup

--- business-card-mobile-app/App.js
+++ business-card-mobile-app/App.js
@@ -1,7 +1,5 @@
 import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React from "react";
 
 // Navigation
 import { NavigationContainer } from "@react-navigation/native";
@@ -15,20 +13,7 @@ import ARUnityScreen from "./screens/augmented-reality";
 const Stack = createStackNavigator();
 
 export default function App() {
-  const [initText, setInitText] = useState("Welcome to the Business Card app!");
   return (
-    // <View style={styles.container}>
-    //   <Text>{initText}</Text>
-    //   <StatusBar style="auto" />
-    //   <Button
-    //     title="Change text"
-    //     onPress={() =>
-    //       setInitText(
-    //         "The ultimate AR AI business card experience... Welcome to the future of business cards."
-    //       )
-    //     }
-    //   />
-    // </View>
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen
@@ -48,56 +33,12 @@ export default function App() {
         <Stack.Screen
           name="ARUnity"
           component={ARUnityScreen}
-          options={{ title: "AR Business Card" }}
+          options={{
+            // title: "AR Business Card"
+            headerShown: false,
+          }}
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-// const HomeScreen = ({ navigation }) => {
-//   return (
-//     <View style={styles.container}>
-//       <Button
-//         title="PUSH THIS BUTTON"
-//         onPress={() =>
-//           navigation.navigate("Screen2", {
-//             mainText: "THIS IS SCREEN 2 WOOOOO!!!",
-//           })
-//         }
-//       />
-//       <Text style={{ color: "#fff" }}>
-//         Notice that the status bar has light text!
-//       </Text>
-//       <StatusBar style="light" />
-//     </View>
-//   );
-// };
-
-const Screen2 = ({ navigation, route }) => {
-  return (
-    <View style={styles.container}>
-      <Button
-        title="SCREEN 2"
-        onPress={() =>
-          navigation.navigate("Home", {
-            mainText: "THIS IS HOME WOOOOO!!!",
-          })
-        }
-      />
-      <Text style={{ color: "#fff" }}>
-        Notice that the status bar has light text!
-      </Text>
-      <StatusBar style="light" />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
